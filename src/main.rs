e0393eee149028ce56a47c116324317c9595c754
Leonid Semenov <ln.sm.study@gmail.com>
2023-09-30 21:22:12 +0600

    Add current dir reading.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
 fn main() {
-    println!("Hello, world!");
+    let dir = std::env::current_dir().expect("Problem getting working directory");
+
+    let dir_name = dir
+        .into_os_string()
+        .into_string()
+        .expect("Problem reading working directory");
+
+    println!("Current working directory");
+    println!("{}", dir_name);
 }
